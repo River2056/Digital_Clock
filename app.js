a6d94db48ebfa7daef6d61fffcceaed316529d44
River2056 <45826383+River2056@users.noreply.github.com>
2019-02-21 05:05:52 +0800

    changed bg-color, font-color, added count days passed feature

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var pass = new Date("Novenber 12, 2018 22:00 GMT+8:00");
+
 function clock() {
     var date = new Date();
     var now_utc = Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(),
@@ -9,6 +11,9 @@ function clock() {
     var secs = fullDate.getSeconds();
     var day = fullDate.getDay(); 
     var date = fullDate.getDate();
+    var diff = fullDate.getTime() - pass.getTime();
+    var daysPass = diff / 1000 / 60 / 60 / 24;
+    daysPass = daysPass.toFixed(5);
 
     if (hours < 10) {
         hours = "0" + hours;
@@ -48,6 +53,8 @@ function clock() {
     document.getElementById("seconds").innerHTML = " : " + secs;
     document.getElementById("day").innerHTML = day;
     document.getElementById("date").innerHTML ="- " + date;
+    document.querySelector(".count").innerHTML = `days passed: ${daysPass}`
+    document.getElementById("title").innerHTML = `${hours} : ${mins} : ${secs}  ${day} - ${date}`
 }
 
 setInterval(clock, 100);
